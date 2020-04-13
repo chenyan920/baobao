c23eb1b82b750b91196559ee694c7da249cbd3ef
chenyan920 <15040803141@163.com>
2020-04-13 19:55:04 +0800

    修正一个由于win7系统返回时间格式不同所引起的bug

--- baobao/dataInf.js
+++ baobao/dataInf.js
@@ -10,11 +10,9 @@ function getLoctateTime(){
     var date = new Date()
     return date.toLocaleTimeString()
 }
-function calculateDayToDay(year,mouth,day){
-    var date = new Date()
+function calculateDayToDay(){
+    var date = new Date
     var date1 = new Date(2017,12,30)
-    var dateString = date.toDateString()
-    var date2 = new Date(dateString.slice(-4),dateString.slice(3,4),dateString.slice(6,8))
-    return parseInt(((date2.getTime()-date1.getTime())/24/60/60/1000)+1)
-
+    var date2 = new Date(date.getFullYear(),date.getMonth()+1,date.getDate())
+    return parseInt(((date2.getTime()-date1.getTime())/24/60/60/1000)+2)
 }
